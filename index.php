f78d494482aefa09bac5dacaaf838191d20f506e
AndreaRomano02 <andy.romano2002@gmail.com>
2023-07-24 14:03:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php  ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.0/css/bootstrap.min.css' />
+  <title>E-Commerce</title>
+</head>
+
+<body>
+  <h1 class="text-center my-3">PET SHOP</h1>
+</body>
+
+</html>
